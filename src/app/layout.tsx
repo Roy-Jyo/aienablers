0d7f33ce9e1778bf6cedd49046a66197466c94f4
J Roy <jroy@Mac-3.modem>
2026-01-13 21:35:16 +1100

    Add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
+    <script
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MKW78ZBX');
+    `,
+  }}
+/>
       <body className="bg-gray-50 text-gray-900 font-sans">
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MKW78ZBX"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+
+
         <Navbar />
         <main>{children}</main>
         <footer className="py-8 text-center text-sm text-gray-500 bg-gray-100 mt-16">
