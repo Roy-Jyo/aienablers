846661baa35cd86ae77d7ebcc08fea2b0fa44ca0
J Roy <jroy@Mac-3.modem>
2026-01-13 22:08:28 +1100

    Fix GTM placement in Next.js layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,34 +14,42 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-
-    <script
-  dangerouslySetInnerHTML={{
-    __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-MKW78ZBX');
-    `,
-  }}
-/>
-      <body className="bg-gray-50 text-gray-900 font-sans">
-
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-MKW78ZBX"
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MKW78ZBX');
+            `,
+          }}
         />
-      </noscript>
+        {/* End Google Tag Manager */}
+      </head>
 
+      <body className="bg-gray-50 text-gray-900 font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MKW78ZBX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
 
         <Navbar />
         <main>{children}</main>
         <footer className="py-8 text-center text-sm text-gray-500 bg-gray-100 mt-16">
-          © {new Date().getFullYear()} <span className="font-medium text-gray-600">DAPAR Technologies, AIEnablers RaaS.</span> All rights reserved.
+          © {new Date().getFullYear()}{' '}
+          <span className="font-medium text-gray-600">
+            DAPAR Technologies, AIEnablers RaaS.
+          </span>{' '}
+          All rights reserved.
         </footer>
       </body>
     </html>
